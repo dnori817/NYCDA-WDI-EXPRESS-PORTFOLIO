61f812cdce4650cba2aaff36aa313a736a3a1b58
David Noriega <dnori817@gmail.com>
2017-05-14 13:47:47 -0400

    added template

--- app.js
+++ app.js
@@ -4,19 +4,66 @@ const app = express();
 app.use(express.static("assets"));
 app.set("view engine", "ejs");
 
+// let menu = [{
+// 			 text: "Contact",
+// 			 href: "#",
+// 		 }, {
+// 			 text: "Portfolio",
+// 			 href: "#",
+// 		 }, {
+// 			 text: "Gallery",
+// 			 href: "/gallery",
+// 		 }, {
+// 			 text: "About",
+// 			 href: "/",
+// 		 }];
+
+// function adjustMenu() {
+//
+// 	if  ( menu.text. toLowerCase === page) {
+//
+//
+// 	}
+// }
+
 app.get("/", function(req, res) {
 	console.log("Serving up homepage...");
 	// res.render("about");
-	res.render("about", {
-		 title: "About Me"
+	res.render("template", {
+		page: "about",
+		title: "About Me",
+		menu: [{
+			 text: "Contact",
+			 href: "#",
+		 }, {
+			 text: "Portfolio",
+			 href: "#",
+		 }, {
+			 text: "Gallery",
+			 href: "/gallery",
+
+		 }],
+
 	  });
 });
+
 
 app.get("/gallery", function(req, res) {
 	console.log("Opening gallery...");
 	// res.render("gallery");
-	res.render("gallery", {
+	res.render("template", {
+		page: "gallery",
 		title: "Gallery",
+		menu: [{
+			text: "Contact",
+			href: "#",
+		}, {
+			text: "Portfolio",
+			href: "#",
+		}, {
+			text: "About",
+			href: "/",
+		}],
 
 	});
 });
